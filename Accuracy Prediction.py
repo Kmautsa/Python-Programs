a13da2cf6216268ed2ccd70806fc92080e52813a
mautsa,Kudzanai <149285204+Kmautsa@users.noreply.github.com>
2024-11-11 13:31:33 -0500

    Update description of program

--- Accuracy Prediction.py
+++ Accuracy Prediction.py
@@ -1,4 +1,7 @@
 __author__="Kudzanai Mautsa"
+#description: Solution designed to analyze the average prediction accuracy of a machine learning model over the last three months.
+#The accuracy scores for the last three months are 85.5%, 90.2%, and 87.3%.
+
 # Accuracy scores for the last three months
 accuracy_scores = [85.5, 90.2, 87.3]
 
