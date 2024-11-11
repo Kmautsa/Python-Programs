88f95d5e8d61706f6211a54519188a9a0c0d935c
mautsa,Kudzanai <149285204+Kmautsa@users.noreply.github.com>
2024-11-11 13:26:08 -0500

    Create Accuracy Prediction.py

---
+++ Accuracy Prediction.py
@@ -0,0 +1,8 @@
+__author__="Kudzanai Mautsa"
+# Accuracy scores for the last three months
+accuracy_scores = [85.5, 90.2, 87.3]
+
+# Calculate the average accuracy
+average_accuracy = ((85.5+90.2+87.3)//3)
+
+print(f"The average prediction accuracy over the last three months is {average_accuracy:.2f}%")
