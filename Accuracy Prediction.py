9fad12e7ffe44bcc251a8993c90d4167f3ff9a93
mautsa,Kudzanai <149285204+Kmautsa@users.noreply.github.com>
2024-11-22 23:55:43 -0500

    Update Accuracy Prediction.py

--- Accuracy Prediction.py
+++ Accuracy Prediction.py
@@ -7,6 +7,6 @@ accuracy_scores = [85.5, 90.2, 87.3]
 
 # Calculate the average accuracy
 average_accuracy = ((85.5+90.2+87.3)//3)    
-print(f"The average prediction accuracy over the last three months is {average_accuracy:.2f}%")
+print(f"The average prediction accuracy over the last three months is {average_accuracy:.2f}%") 
    
  
