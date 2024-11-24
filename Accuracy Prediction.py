5283a0e6feee211098cac84ab5fe18902c55310e
mautsa,Kudzanai <149285204+Kmautsa@users.noreply.github.com>
2024-11-23 23:17:34 -0500

    Update Accuracy Prediction.py

--- Accuracy Prediction.py
+++ Accuracy Prediction.py
@@ -1,4 +1,4 @@
-__author__="Kudzanai Mautsa"
+ __author__="Kudzanai Mautsa"
 #description: Solution designed to analyze the average prediction accuracy of a machine learning model over the last three months.
 #The accuracy scores for the last three months are 85.5%, 90.2%, and 87.3%.
 
@@ -6,7 +6,7 @@ __author__="Kudzanai Mautsa"
 accuracy_scores = [85.5, 90.2, 87.3]
 
 # Calculate the average accuracy
-average_accuracy = ((85.5+90.2+87.3)//3)    
+average_accuracy = ((85.5+90.2+87.3)//3)     
 print(f"The average prediction accuracy over the last three months is {average_accuracy:.2f}%") 
    
  
